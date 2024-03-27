f4c97d958de5f1d8f600ae6efcf784caffe6f6db
lois.roxi <lois.roxi@gmail.com>
2024-03-27 22:00:36 +0100

    1.1 I can play Player vs Computer and 1.2 I can play Computer vs Computer - restructuring

--- RPS/src/RockPaperScissors.java
+++ RPS/src/RockPaperScissors.java
@@ -41,56 +41,10 @@ public class RockPaperScissors {
     public static void main(String[] args) {
 
         //1.1 I can play Player vs Computer
-        //1.1.1 Read player input
-        Scanner scn = new Scanner(System.in);
-        String playerDecision;
-        String computerAction;
-
-        System.out.println("Welcome to the Rock Paper Scissors - Player vs Computer Game");
-        System.out.println("Choose your action: Rock/Paper/Scissors");
-        playerDecision = scn.nextLine();
-
-        // checking if user's move is one of the available moves or not
-        if (playerDecision.equals("Rock") || playerDecision.equals("Paper") || playerDecision.equals("Scissors")) {
-            System.out.println(STR."You have chosen: \{playerDecision}");
-        } else {
-            System.out.println(STR."You have chosen an invalid move: \{playerDecision}");
-        }
-
-        //1.1.2 Implement Computer Result
-        String[] possibleMoves = {"Rock", "Paper", "Scissors"};
-        Random random = new Random();
-        int randomIndex = random.nextInt(possibleMoves.length);
-        String computerDecision = possibleMoves[randomIndex];
-        System.out.println("Computer chosen move: " + computerDecision);
-
-        if (playerDecision.equals("Rock")) {
-            if (computerDecision.equals("Paper"))
-                System.out.println("Computer won!");
-            else if (computerDecision.equals("Scissors"))
-                System.out.println("You won!");
-            else
-                System.out.println("Tie!");
-
-        } else if (playerDecision.equals("Paper")) {
-            if (computerDecision.equals("Scissors"))
-                System.out.println("Computer won!");
-            else if (computerDecision.equals("Rock"))
-                System.out.println("You won!");
-            else
-                System.out.println("Tie!");
-
-        } else if (playerDecision.equals("Scissors")) {
-            if (computerDecision.equals("Rock"))
-                System.out.println("Computer won!");
-            else if (computerDecision.equals("Paper"))
-                System.out.println("You won!");
-            else
-                System.out.println("Tie!");
-        }
+        playPlayerVsComputer();
 
         //1.2 I can play Computer vs Computer
-        //1.2.1 Implement Computer vs Computer
+        playComputerVsComputer();
 
         //1.3 I can play a different game each time
         //1.3.1 Implement choosing menu
@@ -104,4 +58,73 @@ public class RockPaperScissors {
 
     }
 
+    public static void playPlayerVsComputer() {
+        String playerDecision;
+        String computerDecision;
+
+        playerDecision = readPlayerMove();
+        computerDecision = generateComputerDecision();
+        System.out.println("Computer chosen move: " + computerDecision);
+        determineWinner(playerDecision, computerDecision);
+
+    }
+
+    //1.1.1 Read player input
+    public static String readPlayerMove() {
+
+        Scanner scn = new Scanner(System.in);
+        String playerDecision;
+
+        System.out.println("Welcome to the Rock Paper Scissors - Player vs Computer Game!");
+
+        System.out.println("Choose your move: Rock/Paper/Scissors");
+        while (true) {
+            playerDecision = scn.nextLine();
+
+            if (playerDecision.equals("Rock") || playerDecision.equals("Paper") || playerDecision.equals("Scissors")) {
+                System.out.println("You have chosen: " + playerDecision);
+                break;
+            } else {
+                System.out.println("You have chosen an invalid move: " + playerDecision);
+                System.out.println("YPlease choose your move from the available moves: Rock, Paper, Scissors");
+            }
+        }
+        return playerDecision;
+    }
+
+    //1.2.1 Implement Computer vs Computer
+    public static void playComputerVsComputer() {
+
+        //Generate each computer move
+        String computer1Decision = generateComputerDecision();
+        String computer2Decision = generateComputerDecision();
+
+        System.out.println("Computer 1 chosen move: " + computer1Decision);
+        System.out.println("Computer 2 chosen move: " + computer2Decision);
+
+        //Determine the winner based on each moves
+        determineWinner(computer1Decision, computer2Decision);
+    }
+
+    public static String generateComputerDecision() {
+        String[] possibleMoves = {"Rock", "Paper", "Scissors"};
+
+        Random random = new Random();
+        //Generate random index
+        int randomIndex = random.nextInt(possibleMoves.length);
+        //Return the string at the randomly generated index
+        return possibleMoves[randomIndex];
+    }
+
+    public static void determineWinner(String playerMove, String computerMove) {
+        if (playerMove.equals(computerMove)) {
+            System.out.println("Tie!");
+        } else if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+                (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+                (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("Computer won!");
+        }
+    }
 }
